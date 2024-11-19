d4de7435c8f323088700a044163d858ac247d354
Khusnora <126550399+KhusnoraSobirova@users.noreply.github.com>
2024-11-19 16:05:52 +0900

    feat create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { DogService } from './dog.service';
-import {query, Request} from "express";
+import {query} from "express";
 
 @Controller('dog')
 export class DogController {
@@ -22,5 +22,15 @@ export class DogController {
     console.log("query:",query);
     return this.dogService.introduce();
   }
+
+
+
+@Post("edit")
+public  modifyDetail(@Body () body: any): string {
+
+   console.log("body:", body);
+  
+   return this.dogService.modifyDetail();
+ }
   
 }
--- src/dog/dog.service.ts
+++ src/dog/dog.service.ts
@@ -8,4 +8,8 @@ export class DogService {
     public introduce(): string {
         return " My name is Jack!";
     }
+    public modifyDetail(): string {
+        return " Successfully Modified!";
+    }
+
 }
