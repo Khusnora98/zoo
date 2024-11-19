2ca756533866100716e2527715080d39ecdb0791
Khusnora <126550399+KhusnoraSobirova@users.noreply.github.com>
2024-11-19 15:58:16 +0900

    BRR: Initial Setup

---
+++ src/cat/cat.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { CatService } from './cat.service';
+
+
+//localhost:3000/cat
+
+@Controller('cat')
+export class CatController {
+    constructor(private readonly catService: CatService) {}
+
+@Get()
+ public  getHello(): string {
+    return this.catService.getHello();
+  }
+
+  //localhost:3000/cat/greet
+  
+@Get("greet")
+ public  introduce(): string {
+    return this.catService.introduce();
+  }
+  
+}
